29781cf5e04db1dfe4cba8a6e1a83d1da37fff8a
Ensky <enskylin@gmail.com>
2015-08-03 02:08:58 +0800

    remove compiled translations.js

--- www/js/frontend/translations.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
-    gettextCatalog.setStrings('en', {"Close":"Close","Login":"Login"});
-    gettextCatalog.setStrings('zh_TW', {"Close":"關閉","Login":"登入"});
-/* jshint +W100 */
-}]);
