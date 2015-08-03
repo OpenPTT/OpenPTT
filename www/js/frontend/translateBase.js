bff99396ba295b270a8a93e59a17a16a49b35445
sivid <sivid13@gmail.com>
2015-08-03 21:17:41 +0800

    overwrite index-main.html with en.html

--- www/js/frontend/translateBase.js
+++ www/js/frontend/translateBase.js
@@ -7,6 +7,7 @@ lang = lang.toLowerCase().match(/^zh/) ? 'zh_TW' : 'en';
 var translations = ['gettextCatalog', function (gettextCatalog) {
   gettextCatalog.currentLanguage = lang;
   gettextCatalog.debug = true;
+  // gettextCatalog.showTranslatedMarkers = true;
 }];
 
 return translations;
